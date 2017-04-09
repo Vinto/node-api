82868b9440caf07a9b2bde2f8d1ce218161db16f
Vinsent Chitekwe <vinsent@Vinsents-MacBook-Pro.local>
2017-04-09 16:47:02 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -49,8 +51,26 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Started on port 3000');
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	} 
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send();
+		}
+
+		res.status(200).send({todo});
+	}, (e) => {
+		res.status(404).send();
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
